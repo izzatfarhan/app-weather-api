d813abd112b9eba72e9b2d29969d24e8f5279cf6
IzzatFarhan <izzatfrhann@gmail.com>
2026-05-09 12:54:12 +0800

    Testing endpoint

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route("/api/v1/<station>/<date>")
+@app.route('/api/v1/<station>/<date>')
 def about(station, date):
     df = pandas.read_csv("") # read data in csv file
     temperature = df.station(date)  # label temperature, call statiton in df
     return {"station" : station,
             "date" : date,
             "temperature" : temperature}
+
+@app.route('/api/v1/<word>/')
+def page(word):
+    definition = word.upper()
+    return {"definition": definition,
+            "word": word}
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001) # specify port if running multiple apps
